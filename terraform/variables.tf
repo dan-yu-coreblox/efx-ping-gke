79b4e8a21063e4bee32e16e2446d1fd420dfb180
dan-yu-coreblox <71791117+dan-yu-coreblox@users.noreply.github.com>
2020-12-27 14:28:09 -0500

    Update variables.tf
    Draft 3

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -32,3 +32,7 @@ variable "gke_num_nodes" {
   default = 1
   description = "number of gke nodes"
 }
+
+variable "master_ipv4_cidr_block" {
+  description = "Master IPv4 CIDR Block for GKE Cluster"
+}
